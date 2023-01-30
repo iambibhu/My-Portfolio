8a11c945463ce1f6b76f94cef9c604b0172ba22d
Bibhu <bibhuprasad.98@gmail.com>
2023-01-30 16:48:04 +0530

    third commit

--- src/Components/WorkcardData.js
+++ src/Components/WorkcardData.js
@@ -1,29 +1,37 @@
 import pro1 from "../assets/project1.png"
 import pro2 from "../assets/project2.png"
-import pro3 from "../assets/project3.png"
+import pro3 from "../assets/project4.png"
+import pro4 from "../assets/project3.png"
 
 const ProjectcardData = [
     {
         imgsrc: pro1,
         title: "Morden Responsive Website",
-        text: "Fully responsive modern static website using HTML, CSS",
+        text: "Responsive modern static website using HTML, CSS",
         view: "https://iambibhu.github.io/Modern-Responsive-Website",
         source: "https://github.com/iambibhu/Modern-Responsive-Website"
     },
     {
         imgsrc: pro2,
         title: "Food Order Website",
-        text: "Simple food order website using React JS",
+        text: "Simple food order website using React JS, HTML, CSS",
         view: "https://restaurant-bib.netlify.app/",
         source: "https://github.com/iambibhu/Food-Order-Website"
     },
     {
         imgsrc: pro3,
-        title: "Project Title 3",
-        text: "Lorem 3",
-        view: "Project Link",
-        source: "Source code"
-    }
+        title: "Glassmorphic SignUp Page",
+        text: "Responsive Portfolio website using React JS, HTML, CSS",
+        view: "https://signup-bib.netlify.app/",
+        source: "https://github.com/iambibhu/Glassmorphic-SignUp-Page"
+    },
+    {
+        imgsrc: pro4,
+        title: "Glassmorphic SignUp Page",
+        text: "Stylish SignUp page using HTML, CSS",
+        view: "https://signup-bib.netlify.app/",
+        source: "https://github.com/iambibhu/Glassmorphic-SignUp-Page"
+    },
 ];
 
 export default ProjectcardData;
